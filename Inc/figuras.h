05a6fdf30b667c3be9511164ca0cb393d7b418fd
t4v0 <gugvfranca@gmail.com>
2019-11-13 13:36:00 -0300

    Inicializando repositório

---
+++ Inc/figuras.h
@@ -0,0 +1,13 @@
+#ifndef FIGURAS_H_
+#define FIGURAS_H_
+
+#include "NOKIA5110_fb.h"
+//504 bytes por figura - 48x84 pixels
+
+const unsigned char background [504] = { [0 ... 503] = 0xFF };
+
+// ---------------------------------------------------------------------------------------
+// Os pixeis n�o empregados na definic�o dos bytes DEVEM ser ZERO, ou seja, os pixeis que n�o devem ser impressos
+// A figura � desenhada conforme trabalho do LCD, do LSB to MSB (bytes alinhados verticalmente, ver manual do LCD5110)
+//-----------------------------------------------------------------------------------------------------------------
+#endif /* FIGURAS_H_ */
